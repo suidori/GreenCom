ffc86656ff0cac50e0ce882ef674fce57b82671d
suidori <leo7750@naver.com>
2024-07-01 15:42:08 +0900

    commit test

--- JAVA_Work/ex03/src/ex03/test2.java
+++ JAVA_Work/ex03/src/ex03/test2.java
@@ -7,28 +7,24 @@ public class test2 {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("최대공약수 구하기");
+		
 		System.out.println("첫 번째 숫자를 입력하세요");
 		int a = sc.nextInt();
 		System.out.println("첫 번째 숫자 = " + a);
+		
 		System.out.println("두 번째 숫자를 입력하세요");
 		int b = sc.nextInt();
 		System.out.println("두 번째 숫자 = " + b);
+		
 		int gcd = test2.gcd(a, b);
-
 		System.out.println("최대공약수는 " + gcd + "입니다.");
-
 	}
-
 	public static int gcd(int a, int b) {
 		while (b != 0) {
 			int temp = a;
 			a = b;
 			b = temp % b;
-
 		}
-
 		return a;
-
 	}
-
 }
