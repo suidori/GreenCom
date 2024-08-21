b3ebe2d7a6531a26e961926252f72bae6d91234a
suidori <leo7750@naver.com>
2024-08-21 18:09:32 +0900

    0821

--- JAVA_Work/Intellij/CodingTest/src/Test01.java
+++ JAVA_Work/Intellij/CodingTest/src/Test01.java
@@ -2,33 +2,12 @@ import java.util.Arrays;
 
 public class Test01 {
     public static void main(String[] args) {
+        String str1 = "1234";
+        String str2 = new String("1234");
 
-        int[][] arr = scoreBoard(3);
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
 
-        for (int[] ints : arr) {
-            System.out.println(Arrays.toString(ints));
-        }
-    }
-
-    public static int[][] scoreBoard(int n){
-        int[][] arr = new int[n][n];
-        int cycle = 1;
-        int[] y = {0, 1, 0, -1};
-        int[] x = {1, 0, -1, 0};
-        int a = 0;
-        int b = -1;
-
-        while(cycle<=Math.pow(n, 2)){
-            for (int i = 0; i < 4; i++) {
-                while((a+y[i]>=0)&&(b+x[i]>=0)&&(a+y[i]<n)&&(b+x[i]<n)&&(arr[a+y[i]][b+x[i]]==0)) {
-                        arr[a+y[i]][b+x[i]] = cycle;
-                        a += y[i];
-                        b += x[i];
-                        cycle++;
-                }
-            }
-        }
-        return arr;
     }
 
 }
