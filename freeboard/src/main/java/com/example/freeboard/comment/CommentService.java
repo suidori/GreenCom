141af078aaf568768dd081397ee998501040c8c5
suidori <leo7750@naver.com>
2024-09-19 17:59:30 +0900

    0919lastcommit

--- freeboard/src/main/java/com/example/freeboard/comment/CommentService.java
+++ freeboard/src/main/java/com/example/freeboard/comment/CommentService.java
@@ -13,6 +13,9 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     public boolean deleteComment(long fidx, int c_idx, String c_password) {
+        System.out.println(fidx);
+        System.out.println(c_idx);
+        System.out.println(c_password);
         CommentId commentId = new CommentId(c_idx, fidx);//복합 키 생성
         Optional<Comment> optionalComment = commentRepository.findById(commentId); //복합 키 검색
         if (optionalComment.isPresent()) {
@@ -22,7 +25,7 @@ public class CommentService {
                 List<Comment> responseComments = commentRepository.findByFidx(fidx); //같은 게시물에 달린 댓글 검색
 
                 for (Comment reply : responseComments) {
-                    if (reply.getC_response_index() == c_idx) { //대댓글 검색
+                    if (reply.getC_response_index()!=null && reply.getC_response_index() == c_idx) { //대댓글 검색
                         comment.setC_avail(false); //있으면 원댓글 false
                         commentRepository.save(comment); //save
                         return true; //확인용 변수 체크
