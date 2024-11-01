1d221a0df423495484c2d8ee36da26c412731884
suidori <leo7750@naver.com>
2024-11-01 13:52:58 +0900

    1101

---
+++ expo_work/ex01/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+    return (  <View>
+        <Text>
+            Index
+            </Text>
+    </View>  );
+}
+
+export default Index;
